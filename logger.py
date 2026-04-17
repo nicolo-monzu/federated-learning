0270de13dd6e705c059435d79b1094bbc9deef29
Nicolò Lucio Monzù <s315037@studenti.polito.it>
2026-04-17 18:55:00 +0200

    add training resume; add debug mode

--- logger.py
+++ logger.py
@@ -2,33 +2,47 @@ import json
 import os
 
 
+def truncate_file(filepath, n):
+    with open(filepath, 'r') as f:
+        lines = [next(f) for _ in range(n)]
+
+    with open(filepath+'.tmp', 'w') as f:
+        f.writelines(lines)
+    os.replace(filepath+'.tmp', filepath)
+
 class Logger:
     def __init__(self, log_dir, run_name):
         os.makedirs(log_dir, exist_ok=True)
-        self.log_path = f'{log_dir}/log_{run_name}.csv'
+        self.log_path = f'{log_dir}/{run_name}_log.csv'
         self.det_path = f'{log_dir}/{run_name}.json'
 
+    def get_run(self):
+        return self.run
 
-    def start(self, exp):
+    def start(self, run):
+        self.run = run
+        # Create json
         with open(self.det_path, 'w') as f:
-            json.dump(exp, f, indent=4, ensure_ascii=False)
+            json.dump(self.run, f, indent=4, ensure_ascii=False)
+        # Create log
         with open(self.log_path, 'w') as f:
             f.write('epoch,train_loss,val_loss,train_acc,val_acc\n')
 
 
-    def resume(self, exp):
-        #todo
-        pass
+    def resume(self, epoch, best_acc):
+        with open(self.det_path, 'r') as f:
+            self.run = json.load(f)
+        # Restore json
+        self.update_best_acc(best_acc)
+        # Restore log
+        truncate_file(self.log_path, epoch+1)
 
 
     def update_best_acc(self, best_acc):
-        with open(self.det_path, 'r') as f:
-            exp = json.load(f)
-
-        exp['best_accuracy'] = best_acc
-
+        self.run['best_accuracy'] = best_acc
+        # Update json
         with open(self.det_path+'.tmp', 'w') as f:
-            json.dump(exp, f, indent=4, ensure_ascii=False)
+            json.dump(self.run, f, indent=4, ensure_ascii=False)
         os.replace(self.det_path+'.tmp', self.det_path)
 
 
--- train.py
+++ train.py
@@ -6,6 +6,8 @@ from torch import nn
 from data.dataloader import create_dataloaders
 from logger import Logger
 
+DEBUG = False
+
 DEVICE = "cuda" if torch.cuda.is_available() else "cpu"
 
 def save(checkpoint, filename):
@@ -19,9 +21,9 @@ def train_one_epoch(epoch, model, train_loader, criterion, optimizer):
     total = 0
 
     for batch_idx, (inputs, targets) in enumerate(train_loader):
-        # Debugging
-        # if batch_idx > 1:
-        #     break
+        if DEBUG:
+            if batch_idx > 1:
+                break
         inputs, targets = inputs.to(DEVICE), targets.to(DEVICE)
 
         outputs = model(inputs)
@@ -50,9 +52,9 @@ def validate(model, val_loader, criterion):
 
     with torch.no_grad():
         for batch_idx, (inputs, targets) in enumerate(val_loader):
-            # Debugging
-            # if batch_idx > 1:
-            #     break
+            if DEBUG:
+                if batch_idx > 1:
+                    break
             inputs, targets = inputs.to(DEVICE), targets.to(DEVICE)
 
             outputs = model(inputs)
@@ -70,8 +72,8 @@ def validate(model, val_loader, criterion):
     return val_loss, val_accuracy
 
 
-def train(num_epochs, run_name, model, train_loader, val_loader, criterion, optimizer, logger, checkpoints_dir, best_acc):
-    for epoch in range(1, num_epochs + 1):
+def train(num_epochs, run_name, model, train_loader, val_loader, criterion, optimizer, logger, checkpoints_dir, best_acc, start_epoch=1):
+    for epoch in range(start_epoch, num_epochs + 1):
         train_loss, train_acc = train_one_epoch(epoch, model, train_loader, criterion, optimizer)
 
         # At the end of each training iteration, perform a validation step
@@ -85,12 +87,12 @@ def train(num_epochs, run_name, model, train_loader, val_loader, criterion, opti
             'epoch': epoch,
             'model_state_dict': model.state_dict(),
             'optimizer_state_dict': optimizer.state_dict(),
-            'best_acc': best_acc
+            'accuracy': val_acc
         }
         save(checkpoint, f'{checkpoints_dir}/{run_name}_last.pth')
 
         # If it is the best model
-        if val_acc > best_acc:
+        if val_acc > best_acc or epoch == 1:
             # Update best accuracy
             best_acc = val_acc
             logger.update_best_acc(best_acc)
@@ -102,16 +104,74 @@ def train(num_epochs, run_name, model, train_loader, val_loader, criterion, opti
 
     print(f'Best validation accuracy: {best_acc:.2f}%')
 
-def resume():
-    #todo
-    pass
+def resume(run_name, num_epochs):
+    checkpoints_dir = 'centralized_model/checkpoints/'
+    log_dir = 'centralized_model/logs/'
 
-def start():
+    # delete temp files
+    try:
+        os.remove(f'{checkpoints_dir}/{run_name}_last.pt.tmp')
+        os.remove(f'{checkpoints_dir}/{run_name}_best.pth.tmp')
+    except FileNotFoundError:
+        pass
+
+    last = torch.load(f'{checkpoints_dir}/{run_name}_last.pth')
+    best = torch.load(f'{checkpoints_dir}/{run_name}_best.pth')
+
+    # update best checkpoint
+    if last['accuracy'] > best['accuracy']:
+        save(last, f'{checkpoints_dir}/{run_name}_best.pth')
+
+    best_acc = max(last['accuracy'], best['accuracy'])
+    logger = Logger(log_dir=log_dir, run_name=run_name)
+    logger.resume(last['epoch'], best_acc)
+    run = logger.get_run()
+
+    if hasattr(os, 'sync'):
+        os.sync()
+
+    if DEBUG:
+        print('Debug mode')
+    print('Using device:', DEVICE)
+
+    # Import data
+    train_loader, val_loader = create_dataloaders(run['batch_size'])
+
+    # Define model
+    model = torch.hub.load('facebookresearch/dino:main', 'dino_vits16').to(DEVICE)
+
+    criterion = nn.CrossEntropyLoss()
+    optimizer = torch.optim.SGD(model.parameters(), lr=run['learning_rate'], momentum=0.9, weight_decay=run['weight_decay'])
+
+    # Run the training process for {num_epochs} epochs
+    print(f'Run name: {run['name']}')
+    print('Resume training')
+    train(num_epochs, run['name'], model, train_loader, val_loader, criterion, optimizer, logger, checkpoints_dir, best_acc, last['epoch']+1)
+
+def start(num_epochs):
     batch_size = 32
     learning_rate = 0.001
     weight_decay = 1e-4
-    num_epochs = 10
 
+    log_dir='centralized_model/logs'
+    checkpoints_dir = 'centralized_model/checkpoints/'
+    os.makedirs(checkpoints_dir, exist_ok=True)
+
+    # Init logger
+    run = {
+        'name': ('debug_' if DEBUG else '') + datetime.now().strftime('%Y%m%d_%H%M%S'),
+        'model': 'dino_vits16_centralized',
+        'batch_size': batch_size,
+        'learning_rate': learning_rate,
+        'weight_decay': weight_decay,
+        'optimizer': 'SGD(momentum=0.9)',
+        'best_accuracy': 0
+    }
+    logger = Logger(log_dir=log_dir, run_name=run['name'])
+    logger.start(run)
+
+    if DEBUG:
+        print('Debug mode')
     print('Using device:', DEVICE)
 
     # Import data
@@ -123,25 +183,10 @@ def start():
     criterion = nn.CrossEntropyLoss()
     optimizer = torch.optim.SGD(model.parameters(), lr=learning_rate, momentum=0.9, weight_decay=weight_decay)
 
-    # Init logger
-    run = {
-        'name': datetime.now().strftime('%Y%m%d_%H%M%S'),
-        'model': 'dino_vits16_centralized',
-        'batch_size': batch_size,
-        'learning_rate': learning_rate,
-        'weight_decay': weight_decay,
-        'optimizer': 'SGD(momentum=0.9)',
-        'best_accuracy': 0
-    }
-    logger = Logger(log_dir='centralized_model/logs', run_name=run['name'])
-    logger.start(run)
-
-    checkpoints_dir = 'centralized_model/checkpoints/'
-    os.makedirs(checkpoints_dir, exist_ok=True)
-
     # Run the training process for {num_epochs} epochs
+    print(f'Run name: {run['name']}')
     print('Start training')
     train(num_epochs, run['name'], model, train_loader, val_loader, criterion, optimizer, logger, checkpoints_dir, 0)
 
 if __name__ == '__main__':
-    start()
+    start(10 if not DEBUG else 1)
